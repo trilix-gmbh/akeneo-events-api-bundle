6cf5bf3f6aeb5e4b4ed86cbe06775b1ec8c27329
Ruslan Voitenko <psw1986@gmail.com>
2019-10-14 20:06:22 +0200

    APEA-25 Rename RegisterEventTypesPass

--- DependencyInjection/Compiler/RegisterEventTypeConfigurationsPass.php
+++ DependencyInjection/Compiler/RegisterEventTypeConfigurationsPass.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
-class RegisterEventTypesPass implements CompilerPassInterface
+class RegisterEventTypeConfigurationsPass implements CompilerPassInterface
 {
     /**
      * {@inheritdoc}
